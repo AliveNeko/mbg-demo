5f5f031aea8dc04d4420c0c7f08898effa31abd5
LiXin-Link <1191792984@qq.com>
2022-04-19 23:28:11 +0800

    【修正】主键生成修改

--- src/main/java/com/power/mapper/IsolatedInfoMapper.java
+++
@@ -1,7 +0,0 @@
-package com.power.mapper;
-
-import com.power.entity.manage.IsolatedInfo;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface IsolatedInfoMapper extends Mapper<IsolatedInfo> {
-}
